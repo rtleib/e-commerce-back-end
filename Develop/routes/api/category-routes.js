0dcff2744c91a58c46cea1b99fe6b3ef810d31f9
rtleib <rtleib@gmail.com>
2022-05-05 23:34:43 -0500

    added router functions for category find all and find one

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,11 +6,32 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [Product],
+  })
+  .then((categories) => {
+    res.json(categories)
+  })
+  .catch((error) => {
+    res.status(500).json(error)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  })
+  .then((category) => {
+    res.json(category)
+  })
+  .catch((error) => {
+    res.status(500).json(error)
+  })
 });
 
 router.post('/', (req, res) => {
